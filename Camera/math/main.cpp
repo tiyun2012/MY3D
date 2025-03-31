9ca0458333d6f1ef460fb6ddc63793ded2be59d3
tiyun2012 <hoathinhvietnam2016@gmail.com>
2025-04-01 00:34:00 +0700

    matrix

--- Camera/math/main.cpp
+++ Camera/math/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include "Vector3.h"
-
+#include "matrix4.h"
+// #include <cmath>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 int main() {
     // Test using scalar multiplication with the scalar on the left
-    Vector3 v1 =Vector3(1.3f, 3.0f, 5.0f);
-    Vector3 v2=-v1;
-    std::cout << "3.0 * Vector3(1.3, 3, 5) = ("
-              << v2.x << ", " << v2.y << ", " << v2.z << ")\n";
-    
-    // Other tests...
-    // ddddddddddd
+    Vector3 v1 =Vector3(1.0f, 1.0f, 0.0f);
+    Vector3 v2 =Vector3(-1.0f, 0.0f, 0.0f);
+    Vector3 v3 =v1.projectOnto(v2);
+    std::cout << "v1.projectOnto v2 "<<v3.x<< ","<<v3.y<<","<<v3.z<< std::endl;
+    Matrix4 mat=Matrix4();
+    std::cout << "Matrix4 Identity Matrix: " << std::endl << mat << std::endl;
+
     return 0;
 }
