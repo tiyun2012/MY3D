af2e114cc65dc3421f3e746d7e5376b4a63dfed4
Nhat <hoathinhvietnam2016.com>
2025-05-30 11:15:08 +0700

    refactory structure o hierachy

---
+++ ti3D/renderer/Renderer.cpp
@@ -0,0 +1,6 @@
+#include "Renderer.h"
+
+namespace Ti3D {
+// Empty for now, as Renderer methods are defined in Renderer.h
+// Add implementation here if Renderer methods are moved out of header in the future
+}
