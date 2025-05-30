b0d782a6353aaef48537a5333e27adf9a30006f4
Nhat <hoathinhvietnam2016.com>
2025-05-30 13:09:51 +0700

    update code

---
+++ ti3D/app/main.cpp
@@ -0,0 +1,74 @@
+#include "../camera/Camera.h"
+#include "../renderer/Renderer.h"
+#include <iostream>
+
+namespace Ti3D {
+
+static void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+    Ti3D::Camera* cam = static_cast<Ti3D::Camera*>(glfwGetWindowUserPointer(window));
+    if (cam) {
+        cam->setAspectRatio(static_cast<float>(width), static_cast<float>(height));
+    }
+}
+
+} // namespace Ti3D
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Ti3D", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    Ti3D::Camera camera;
+    Ti3D::Renderer renderer;
+    glEnable(GL_DEPTH_TEST);
+
+    glfwSetFramebufferSizeCallback(window, Ti3D::framebufferSizeCallback);
+    glfwSetWindowUserPointer(window, &camera);
+
+    float lastFrame = glfwGetTime();
+    while (!glfwWindowShouldClose(window)) {
+        float currentFrame = glfwGetTime();
+        float deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        camera.processInput(window, deltaTime);
+        double xpos, ypos;
+        glfwGetCursorPos(window, &xpos, &ypos);
+        camera.processMouseInput(window, -xpos, ypos);
+        camera.update(deltaTime); // Enable automatic rotation
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        renderer.drawAxes(camera);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
